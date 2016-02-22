d657de3bdcb4732de3ffb4922170967deb3f0a47
daporte3 <daporte3@asu.edu>
2016-02-22 14:30:31 -0700

    The initial version.

---
+++ src/cse360assign3/Calculator..java
@@ -0,0 +1,67 @@
+/**
+ * @author Douglas Porter
+ * @version 1.0, 2/22/2016
+ * @description The calculator class, performs basic calculator operations on numbers
+ */
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * @category constructor
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * @param none
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * @return void
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * @return void
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * @return void
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * @return void
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * @param none
+	 * @return history
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ src/cse360assign3/calculatorTest.java
@@ -0,0 +1,59 @@
+/**
+ * @author Douglas Porter
+ * @version 1.0, 2/22/2016
+ * @description Test cases for the Calculator.java class
+ */
+
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class calculatorTest {
+
+	@Test
+	public void testCalculator() 
+	{
+		Calculator test1 = new Calculator();
+		
+		assertNotNull(test1);
+	}
+	
+	@Test
+	public void testGetTotal()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSubtract()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDivide()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetHistory()
+	{
+		fail("Not yet implemented");
+	}
+
+}
